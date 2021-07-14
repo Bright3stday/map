2a94b56b9b03153a429b794bbf3537382c8743ae
Anwar <anwartan8@gmail.com>
2021-07-14 17:40:07 +0800

    updated map.py, import modules on separate lines

--- map.py
+++ map.py
@@ -1,13 +1,17 @@
 #! python3
 # map.py -Launch a google map in the browser using address from the clipboard or command line
-"""webbrowser,pyperclip and system module"""
-import webbrowser, sys, pyperclip
+"""webbrowser module"""
+import webbrowser
+"""system module"""
+import sys 
+"""pyperclip module"""
+import pyperclip
 if len(sys.argv)> 1:
     #address from command line
-    address = ''.join(sys.argv[1:])
+    Address = ''.join(sys.argv[1:])
 else:
     #address from clipboard
-    address = pyperclip.paste()
+    Address = pyperclip.paste()
 
-webbrowser.open('https://www.google.com/maps/place/' + address)
+webbrowser.open('https://www.google.com/maps/place/' + Address)
 
