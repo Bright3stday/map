67728d18ba8831467e4394fd3556209592d37dd1
Anwar <anwartan8@gmail.com>
2021-07-14 18:33:23 +0800

    updated formatting

--- map.py
+++ map.py
@@ -4,11 +4,11 @@ import webbrowser
 
 import pyperclip
 
-if len(sys.argv)> 1:
-    #address from command line
+if len(sys.argv) > 1:
+    # address from command line
     ADDRESS = ''.join(sys.argv[1:])
 else:
-    #address from clipboard
+    # address from clipboard
     ADDRESS = pyperclip.paste()
 
 webbrowser.open('https://www.google.com/maps/place/' + ADDRESS)
